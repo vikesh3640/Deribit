d1ef0edb65309bf46d06b67dd22a35aeaa5fcb6d
vikesh3640 <vikeshkumar3640@gmail.com>
2025-04-28 20:38:54 +0530

    Updated WebSocket server code and submodule reference

--- src/websocket/websocket_server.cpp
+++ src/websocket/websocket_server.cpp
@@ -1,24 +1,24 @@
-#include <uWebSockets/App.h>
-#include <iostream>
-#include <string_view>
+// #include <uWebSockets/App.h>
+// #include <iostream>
+// #include <string_view>
 
-struct UserData {}; 
+// struct UserData {}; 
 
-void runWebSocketServer() {
-    std::cout << "Starting WebSocket server on port 9001..." << std::endl;
+// void runWebSocketServer() {
+//     std::cout << "Starting WebSocket server on port 9001..." << std::endl;
 
-    uWS::App().ws<UserData>("/*", {
-        .open = [](auto* ws) {
-            std::cout << "A client connected!" << std::endl;
-        },
-        .message = [](auto* ws, std::string_view message, uWS::OpCode opCode) {
-            std::cout << "Received message: " << message << std::endl;
-            ws->send(message, opCode); 
-        },
-        .close = [](auto* ws, int code, std::string_view message) {
-            std::cout << "A client disconnected!" << std::endl;
-        }
-    });
+//     uWS::App().ws<UserData>("/*", {
+//         .open = [](auto* ws) {
+//             std::cout << "A client connected!" << std::endl;
+//         },
+//         .message = [](auto* ws, std::string_view message, uWS::OpCode opCode) {
+//             std::cout << "Received message: " << message << std::endl;
+//             ws->send(message, opCode); 
+//         },
+//         .close = [](auto* ws, int code, std::string_view message) {
+//             std::cout << "A client disconnected!" << std::endl;
+//         }
+//     });
     // .listen(9001, [](auto* listen_socket) {
     //     if (listen_socket) {
     //         std::cout << "Listening on port 9001" << std::endl;
@@ -26,9 +26,9 @@ void runWebSocketServer() {
     //         std::cout << "Failed to listen on port 9001" << std::endl;
     //     }
     // }).run();
-}
+// }
 
-int main() {
-    runWebSocketServer();
-    return 0;
-}
+// int main() {
+//     runWebSocketServer();
+//     return 0;
+// }
--- src/websocket/websocket_server.h
+++ src/websocket/websocket_server.h
@@ -1,3 +1,3 @@
-#pragma once
+// #pragma once
 
-void runWebSocketServer();
+// void runWebSocketServer();
